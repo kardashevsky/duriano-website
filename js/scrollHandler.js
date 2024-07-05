73eac95f78fc4dcae8a3e973103c532ba4e70209
Dmitry Kardashevsky <kardashevskydv@gmail.com>
2024-07-05 23:07:01 +0300

    fix: styles and text

--- js/scrollHandler.js
+++ js/scrollHandler.js
@@ -5,6 +5,8 @@ export default function handleScroll() {
   const playButtonFooter = document.querySelector('.playButtonFooter');
   const howToHeaderEng = document.querySelector('.howToHeaderEng');
   const trailerHeaderEng = document.querySelector('.trailerHeaderEng');
+  const windowTrailerVideo = document.querySelector('.windowTrailerVideo');
+  const playVideoButton = document.querySelector('.playVideoButton');
   const roadmapHeaderEng = document.querySelector('.roadmapHeaderEng');
   const scrollPosition = window.scrollY + window.innerHeight;
   const threshold = document.documentElement.scrollHeight - window.innerHeight;
@@ -48,15 +50,27 @@ export default function handleScroll() {
     }
   }
 
-    // Логика для roadmapHeaderEng
-    const pixelThreshold3 = 2600; // Укажите пороговое значение прокрутки в пикселях
-    if (roadmapHeaderEng) {
-      if (window.scrollY >= pixelThreshold3) {
-        roadmapHeaderEng.classList.add('visible');
-      } else {
-        roadmapHeaderEng.classList.remove('visible');
-      }
+  // Логика для windowTrailerVideo
+  const pixelThreshold3 = 1850; // Укажите пороговое значение прокрутки в пикселях
+  if (windowTrailerVideo) {
+    if (window.scrollY >= pixelThreshold3) {
+      windowTrailerVideo.classList.add('visible');
+      playVideoButton.classList.add('visible');
+    } else {
+      windowTrailerVideo.classList.remove('visible');
+      playVideoButton.classList.remove('visible');
     }
+  }
+
+  // Логика для roadmapHeaderEng
+  const pixelThreshold4 = 2600; // Укажите пороговое значение прокрутки в пикселях
+  if (roadmapHeaderEng) {
+    if (window.scrollY >= pixelThreshold4) {
+      roadmapHeaderEng.classList.add('visible');
+    } else {
+      roadmapHeaderEng.classList.remove('visible');
+    }
+  }
 }
 
 // Подключаем обработчик прокрутки
