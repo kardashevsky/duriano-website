c79172742f2233db94263653d94cdb978d536201
Dmitry Kardashevsky <kardashevskydv@gmail.com>
2024-07-06 23:55:31 +0300

    add: animations and language switcher

--- js/languageSwitchers.js
+++ js/languageSwitchers.js
@@ -1,30 +1,64 @@
 // js/languageSwitchers.js
 export default function initializeLanguageSwitchers() {
   const languageSwitchers = document.querySelectorAll('.language-switcher');
-  const p2eTextEng = document.querySelector('.p2eTextEng');
-  const p2eTextCn = document.querySelector('.p2eTextCn');
-  const telegramChannelLink = document.getElementById('telegramChannelLink');
+  
+  const elements = {
+    p2eText: {
+      eng: document.querySelector('.p2eTextEng'),
+      cn: document.querySelector('.p2eTextCn')
+    },
+    telegramChannelLink: document.getElementById('telegramChannelLink'),
+    howToHeader: {
+      eng: document.querySelector('.howToHeaderEng'),
+      cn: document.querySelector('.howToHeaderCn')
+    },
+    trailerHeader: {
+      eng: document.querySelector('.trailerHeaderEng'),
+      cn: document.querySelector('.trailerHeaderCn')
+    },
+    roadmapHeader: {
+      eng: document.querySelector('.roadmapHeaderEng'),
+      cn: document.querySelector('.roadmapHeaderCn')
+    },
+    roadMapQ3Text: {
+      eng: document.querySelector('.roadMapQ3TextEng'),
+      cn: document.querySelector('.roadMapQ3TextCn')
+    },
+    roadMapQ4Text: {
+      eng: document.querySelector('.roadMapQ4TextEng'),
+      cn: document.querySelector('.roadMapQ4TextCn')
+    },
+    roadMapQ1Text: {
+      eng: document.querySelector('.roadMapQ1TextEng'),
+      cn: document.querySelector('.roadMapQ1TextCn')
+    },
+    cnLanguageImage: document.querySelector('.cnLanguage'),
+    engLanguageImage: document.querySelector('.engLanguage')
+  };
 
   languageSwitchers.forEach(function(switcher) {
     switcher.addEventListener('click', function() {
       const lang = switcher.getAttribute('data-lang');
-      const cnLanguageImage = document.querySelector('.cnLanguage');
-      const engLanguageImage = document.querySelector('.engLanguage');
+
+      // Общая функция для скрытия/отображения элементов
+      function toggleVisibility(lang) {
+        const elementsToToggle = ['p2eText', 'howToHeader', 'trailerHeader', 'roadmapHeader', 'roadMapQ3Text', 'roadMapQ4Text', 'roadMapQ1Text'];
+        elementsToToggle.forEach(element => {
+          elements[element].eng.classList.toggle('hidden', lang !== 'eng');
+          elements[element].cn.classList.toggle('hidden', lang !== 'cn');
+        });
+      }
 
       if (lang === 'cn') {
-        cnLanguageImage.src = 'assets/languageSwitcher/cnSwitchOn.svg';
-        engLanguageImage.src = 'assets/languageSwitcher/engSwitchOff.svg';
-
-        p2eTextEng.classList.add('hidden');
-        p2eTextCn.classList.remove('hidden');
-        telegramChannelLink.href = 'https://t.me/DurianoChina';
+        elements.cnLanguageImage.src = 'assets/languageSwitcher/cnSwitchOn.svg';
+        elements.engLanguageImage.src = 'assets/languageSwitcher/engSwitchOff.svg';
+        elements.telegramChannelLink.href = 'https://t.me/DurianoChina';
+        toggleVisibility('cn');
       } else if (lang === 'eng') {
-        cnLanguageImage.src = 'assets/languageSwitcher/cnSwitchOff.svg';
-        engLanguageImage.src = 'assets/languageSwitcher/engSwitchOn.svg';
-
-        p2eTextEng.classList.remove('hidden');
-        p2eTextCn.classList.add('hidden');
-        telegramChannelLink.href = 'https://t.me/DurianoTeam';
+        elements.cnLanguageImage.src = 'assets/languageSwitcher/cnSwitchOff.svg';
+        elements.engLanguageImage.src = 'assets/languageSwitcher/engSwitchOn.svg';
+        elements.telegramChannelLink.href = 'https://t.me/DurianoTeam';
+        toggleVisibility('eng');
       }
     });
   });
